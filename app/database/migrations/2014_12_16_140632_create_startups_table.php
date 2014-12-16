8c3bec7a1fcc1c7a2139148ff53521bad7c987bb
Dejan Angelov <angelovdejan92@gmail.com>
2014-12-16 15:25:23 +0100

    migrations for the basic tables

---
+++ app/database/migrations/2014_12_16_140632_create_startups_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStartupsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('startups', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->string('name');
+            $table->integer('founded');
+            $table->string('url');
+            $table->string('twitter');
+            $table->string('logo');
+            $table->string('contact_name')->nullable();
+            $table->string('contact_email')->nullable();
+            $table->boolean('approved')->default(false);
+            $table->boolean('featured')->default(false);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('startups');
+	}
+
+}
