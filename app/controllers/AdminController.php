07086fab299728c2f087ddf8c0ed57efbecb20da
Dejan Angelov <angelovdejan92@gmail.com>
2014-12-17 16:35:13 +0100

    admin panel home page

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -1,9 +1,13 @@
 <?php
 
+use G6\FoundedInMk\Entities\Startup;
+
 class AdminController extends BaseController
 {
     public function index()
     {
-        return View::make('admin.index');
+        $approved = Startup::where('approved', true)->count();
+        $waiting = Startup::where('approved', false)->count();
+        return View::make('admin.index', compact('approved', 'waiting'));
     }
 } 
